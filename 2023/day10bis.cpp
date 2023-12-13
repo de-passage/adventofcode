45a827c9e32b0f4d2b5dd238239cad0e12369d26
de-passage <contact@sylvainleclercq.com>
2023-12-13 17:13:33 +0100

    Implements shoelace formula, seems correct

--- 2023/day10bis.cpp
+++ 2023/day10bis.cpp
@@ -2,6 +2,8 @@
 #include "utils.hpp"
 #include <unordered_map>
 #include <unordered_set>
+#include <cmath>
+#include <numeric>
 
 using namespace std;
 using namespace dpsg;
@@ -122,74 +124,21 @@ void print_maze(ostream& out, vector<string> maze, coord s) {
   }
 }
 
-int main(int argc, const char **argv) {
-  auto file = get_input("day10.txt", argc, argv);
-
-  vector<string> maze;
-  string line;
-  coord s{-1, -1};
-
-  size_t x = 0;
-  while (getline(file, line)) {
-    maze.push_back(line);
-    for (size_t i = 0; i < line.size(); i++) {
-      if (line[i] == 'S') {
-        s = {static_cast<int>(x), static_cast<int>(i)};
-      }
-    }
-    ++x;
-  }
-
+void doit(const std::vector<string>& maze, coord s) {
   coord last = {};
-  unordered_set<coord> to_visit;
-  unordered_set<coord> visited;
+  vector<coord> vertices;
+  size_t steps = 0;
   unordered_set<coord> loop;
   do {
     auto p = at(s, maze);
     loop.insert(s);
+    if (p != '-' && p != '|') {
+      vertices.push_back(s);
+    }
+    steps++;
     auto c = direction_from_char(p);
     logln("Currently at ", s, " ", c, " (", p, ")");
     logln([&](ostream& out) { print_maze(out, maze,  s); });
-
-    // compute the cell inside the maze based on the direction we're going
-    // we're going clockwise, and only full cells count, so we can simply
-    // add the cells that are on the right (direction-wise) of the current
-    // straight line
-    if (p == '-') {
-      log("Going horizontally ");
-      if (last == s + coord{0, 1}) {
-        auto i = s + coord{-1, 0};
-        to_visit.insert(i);
-        logln("from right to left, inserting the cell above (", i, ")");
-      } else if (last == s + coord{0, -1}) {
-        auto i = s + coord{1, 0};
-        logln("from left to right, inserting the cell below (", i, ")");
-        to_visit.insert(i);
-      }
-    } else if (p == '|') {
-      log("Going vertically ");
-      if (last == s + coord{1, 0}) {
-        auto i = s + coord{0, 1};
-        logln("from bottom to top, inserting the cell on the right (", i, ")");
-        to_visit.insert(i);
-      } else if (last == s + coord{-1, 0}) {
-        auto i = s + coord{0, -1};
-        logln("from top to bottom, inserting the cell on the left (", i, ")");
-        to_visit.insert(i);
-      }
-    }/*  else if (p == 'J') { // For the remaining cases, we have 2 possible cases, yielding 1 or 2 cells to visit depending on the shape of the corner
-      if (last == s + coord{-1, 0}) {
-        log("Going from top to up")
-      }
-    } else if (p == '7') {
-      log("Going from top to left ");
-    } else if (p == 'L') {
-      log("Going from bottom to right ");
-    } else if (p == 'F') {
-      log("Going from bottom to left ");
-    } */
-    visited.insert(s);
-
 
     // print_maze(maze, s);
     const auto l = s;
@@ -215,61 +164,98 @@ int main(int argc, const char **argv) {
     }
     if (l == s) {
       std::cerr << "Stuck at " << l << " " << c << std::endl;
-      return 1;
+      exit(1);
     }
   } while (maze[s.x][s.y] != 'S');
 
-  logln("Cells to visit (", to_visit.size(), "):", [&](auto &os) {
-      for (auto it = to_visit.begin(); it != to_visit.end(); ++it) {
-      if (it != to_visit.begin()) { os << ", "; }
-        os << *it;
-      }
-  });
+  // Remove start is it's not a vertex
+  if ((vertices[0].x == vertices[1].x && vertices[0].x == vertices[vertices.size() - 1].x)
+      || (vertices[0].y == vertices[1].y && vertices[0].y == vertices[vertices.size() - 1].y)) {
+    vertices.erase(vertices.begin());
+  }
 
-  unordered_set<coord> inside_loop;
-  // We have the list of cells to visit and the list of cells forming the loop,
-  // we can now flood fill the maze to find the cells that are inside the loop
-  // but not part of the path
-  while(!to_visit.empty()) {
-    auto c = *to_visit.begin();
-    to_visit.erase(c);
-    if (visited.find(c) != visited.end()) { // we may have path cells in the to_visit list
+  auto sum = 0;
+  for (auto i = 1u; i < vertices.size(); i++) {
+    auto& v1 = vertices[i - 1];
+    auto& v2 = vertices[i];
+    sum += abs(v1.x*v2.y - v1.y*v2.x);
+  }
+  sum += abs(vertices[vertices.size() - 1].x*vertices[0].y - vertices[vertices.size() - 1].y*vertices[0].x);
+
+  auto sum_f = 0;
+  for (auto i = 1u; i < vertices.size(); i++) {
+    auto& v1 = vertices[i - 1];
+    auto& v2 = vertices[i];
+    sum_f += v1.x*v2.y - v1.y*v2.x;
+  }
+  sum_f += vertices[vertices.size() - 1].x*vertices[0].y - vertices[vertices.size() - 1].y*vertices[0].x;
+
+  auto sum_b = 0;
+  for (auto i = vertices.size() - 1; i > 0; --i) {
+    auto& v1 = vertices[i];
+    auto& v2 = vertices[i - 1];
+    sum_b += v1.x*v2.y - v1.y*v2.x;
+  }
+  sum_b += vertices[0].x*vertices[vertices.size() - 1].y - vertices[0].y*vertices[vertices.size() - 1].x;
+
+  for (auto i = 0u; i < maze.size(); i++) {
+    for (auto j = 0u; j < maze[i].size(); j++) {
+      if (loop.find({static_cast<int>(i), static_cast<int>(j)}) == loop.end()) {
+        std::cout << '.';
+      } else {
+        std::cout << maze[i][j];
+      }
+    }
+    std::cout << std::endl;
+  }
+  double area = double(sum) / 2.;
+  double area_f = double(sum_f) / 2.;
+  double area_b = double(sum_b) / 2.;
+  std::cout << "Found " << vertices.size() << " vertices" << std::endl; logln(vertices);
+  std::cout << "Area (abs): " << area << " Sq root: " << std::sqrt(area) << std::endl;
+  std::cout << "Area (forward): " << area_f << " Sq root: " << std::sqrt(area_f) << std::endl;
+  std::cout << "Area (backward): " << area_b << " Sq root: " << std::sqrt(area_b) << std::endl;
+  std::cout << "Steps: " << steps << std::endl;
+  std::cout << "Area minus steps: " << (abs(area_f) - steps) << std::endl;
+  auto grid = (abs(area_f) - (double(steps)/2.));
+  std::cout << "Attempt: " << grid << " Sq root: " << sqrt(grid) << std::endl;
+  std::cout << "Grid size: " << maze.size() << "x" << maze[0].size() << " == " << maze.size() * maze[0].size()<< std::endl;
+}
+
+int main(int argc, const char **argv) {
+  auto file = get_input("day10.txt", argc, argv);
+
+  vector<string> maze;
+  string line;
+  coord s{-1, -1};
+
+  size_t x = 0;
+  while (getline(file, line)) {
+    if (line.empty()) {
+      if (s.x == -1 && s.y == -1) {
+        continue;
+      } else {
+        doit(maze, s);
+      }
+      maze.clear();
+      s = {-1, -1};
+      x = 0;
       continue;
     }
-    visited.insert(c);
-    inside_loop.insert(c);
-
-    for (auto d : {coord{-1, 0}, coord{0, -1}, coord{1, 0}, coord{0, 1}}) {
-      auto n = c + d;
-      if (!valid(n, maze)) {
-        continue;
-      }
-      if (visited.find(n) != visited.end()) { // already visited (or in the path)
-        continue;
-      }
-      to_visit.insert(n);
-    }
-  }
-
-  auto c = 0;
-  for (size_t y = 0; y < maze.size(); y++) {
-    for (size_t x = 0; x < maze[y].size(); x++) {
-      if (inside_loop.find(coord{static_cast<int>(y), static_cast<int>(x)}) != inside_loop.end()) {
-        cout << "I";
-      } else if (loop.find(coord{static_cast<int>(y), static_cast<int>(x)}) != loop.end()) {
-        cout << maze[y][x];
-      } else {
-        cout << ".";
-        c++;
+    maze.push_back(line);
+    for (size_t i = 0; i < line.size(); i++) {
+      if (line[i] == 'S') {
+        s = {static_cast<int>(x), static_cast<int>(i)};
       }
     }
-    cout << endl;
+    ++x;
   }
-  std::cout << "Found " << inside_loop.size() << " cells inside the loop" << endl;
-  std::cout << "Found " << loop.size() << " cells on the path" << endl;
-  std::cout << "Found " << c << " cells outside the loop" << endl;
-  std::cout << "Total grid size: " << maze.size() << "x" << maze[0].size() << " = " << maze.size() * maze[0].size() << endl;
 
+  if (s.x == -1 && s.y == -1) {
+    // do nothing
+  } else if (!maze.empty()) {
+    doit(maze, s);
+  }
 
   return 0;
 }
