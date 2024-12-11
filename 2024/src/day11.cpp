d46d4e8c2270fad50e33d57241b46ded3f6d6dd0
de-passage <contact@sylvainleclercq.com>
2024-12-11 06:58:27 +0100

    Day 11

---
+++ 2024/src/day11.cpp
@@ -0,0 +1,45 @@
+#include "utils.hpp"
+#include <cmath>
+#include <print>
+
+int digit_count(long t) {
+  int count = 0;
+  while (t > 0) {
+    count++;
+    t /= 10;
+  }
+  return count;
+}
+
+DPSG_AOC_MAIN(file) {
+  std::string input;
+  std::getline(file, input);
+
+  std::vector<size_t> numbers;
+  for (auto i : dpsg::numbers(input)) {
+    numbers.push_back(i);
+  }
+
+  for (int i = 0; i < 25; ++i) {
+    std::vector<size_t> next_numbers;
+    next_numbers.reserve(numbers.size() * 2);
+
+    for (auto n : numbers) {
+      if (n == 0) {
+        next_numbers.push_back(1);
+      } else {
+        int c = digit_count(n);
+        if (c % 2 == 0) {
+          int off = std::pow(10, c / 2);
+          next_numbers.push_back(n / off);
+          next_numbers.push_back(n % off);
+        } else {
+          next_numbers.push_back(n * 2024);
+        }
+      }
+    }
+    numbers = std::move(next_numbers);
+  }
+
+  std::println("Result: {}", numbers.size());
+}
