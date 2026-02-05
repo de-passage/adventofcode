9d1d1150cae69eb69a756e8538c7e45d10ce31fc
Sylvain Leclercq <contact@sylvainleclercq.com>
2026-02-05 06:35:59 +0000

    Stub for day 2

---
+++ 2025/src/day2.cpp
@@ -0,0 +1,10 @@
+#include "utils.hpp"
+#include "print.hpp"
+
+DPSG_AOC_MAIN(file) {
+    std::string line;
+    while (std::getline(file, line)) {
+    
+    }
+    dpsg::println("Answer: {}", 0);
+}
